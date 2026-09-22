198b46993e5fd2ebec25aeb4c3ff8d104e920108
Qaevix <atakan.bitim@gmail.com>
2026-09-22 14:53:57 +0300

    Start C++ Programming course

---
+++ Week02/Week02.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+/*
+
+int count = 5;
+float price = 0.25;
+double total = count * price;
+char --> int --> float --> double
+
+*/
+
+//...
+int a, b, res;
+float k, m;
+//...
+
+printf("Hello World!");
+printf("Variable a value is %i", a);
+printf("Variable k value is %f", k);
+printf("Variable m value is %.3f", m);
+printf("%i + %i = %i", a, b, res);
+//...
+ 
+return 0;
+
+}
