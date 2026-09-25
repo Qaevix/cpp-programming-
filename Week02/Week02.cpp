08e02ba6c61c0f201d722ebfc848e05ac8dc25d2
Qaevix <atakan.bitim@gmail.com>
2026-09-25 23:54:55 +0300

    Week02.cpp Update

--- Week02/Week02.cpp
+++ Week02/Week02.cpp
@@ -3,18 +3,60 @@ using namespace std;
 
 int main(){
 
-//...
+/*
 int a, b, res;
 float k, m;
-//...
 
 printf("Hello World!");
 printf("Variable a value is %i", a);
 printf("Variable k value is %f", k);
 printf("Variable m value is %.3f", m);
 printf("%i + %i = %i", a, b, res);
-//...
- 
+*/
+
+int x = 5;
+string z = "cpp";
+auto y = 10;
+
+	cout << x + y << endl;
+	cout << z;
+	cout << "\nHello world!n\ 2nd week of Programming lesson";
+
+cout << R"(
+
+		***********  ************ ************
+		|	  |  |          | | 	     |
+		|   CPP   |  |  Week02  | | 24.09.26 |
+		|         |  |          | |          |
+		***********  ***********  ************
+
+)";
+
+//printf"("Hello \n");
+
+/*
+int a = 10;
+int b = 3;
+*/
+
+int a = 10, b = 3;
+float c;
+c = 3.17;
+
+float sum = a + c;
+
+//printf("%i", a)
+
+	printf("\nVariable a value is %i \n", a);
+	printf("\nVariable a value is %i \n", b);
+	printf("\nVariable a value is %.10f \n", c);
+	printf("\nSum of a and c is  %.2f \n", sum);
+
+	cout << a << " + " << c << " = " << sum << endl;
+	printf("\n%i + %f = %f \n", a, c , sum);
+
+
 return 0;
 
 }
+
